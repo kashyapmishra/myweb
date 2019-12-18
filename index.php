aeb63d0a1527a57d5d6017ae609d759becac7926
unknown <kashyapmishra666@gmail.com>
2019-12-18 18:47:43 +0530

    k

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+    <title>login</title>
+    </head>
+    <body>
+        <!--
+        <h1>welcome</h1>
+        <p>This HTML tutorial contains hundreds of HTML examples.
+
+            With our online HTML editor, you can edit the HTML, and click on a button to view the result</p>
+            want to
+            <a href="https://www.w3schools.com/html/default.asp">learn html</a> -->
+            <form method="post" action="check_login.php">
+                <lebel>enter name</lebel><br>
+                <input type="text" name="name"><br>
+                <lebel>enter email id</lebel><br>
+                <input type="email" name="emailID"><br>
+                <lebel>enter password</lebel><br>
+                <input type="password" name="password"><br>
+                <input type="submit" value="login"><br>
+                <label >OR</label>
+
+                <a href="signup.php"><button type="button">Click Me!</button></a>
+                </form>
+
+            
+    </body>
+    </html>
---
+++ web.php
@@ -0,0 +1,15 @@
+<?php
+<html>
+<head>
+<title>myweb</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <p>This HTML tutorial contains hundreds of HTML examples.
+
+        With our online HTML editor, you can edit the HTML, and click on a button to view the result</p>
+        want to
+        <a href="https://www.w3schools.com/html/default.asp">learn html</a>
+</body>
+</html>
+?>
